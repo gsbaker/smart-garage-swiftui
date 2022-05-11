4158d9467239b96c2f39d8834e0ab8b52c26a4ce
gsbaker <gsbaker2000@icloud.com>
2022-05-11 11:05:40 +0100

    Initial Commit

---
+++ SmartGarage/SmartGarageApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartGarageApp.swift
+//  SmartGarage
+//
+//  Created by George Baker on 11/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct SmartGarageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
